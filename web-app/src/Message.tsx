f3156212e6a30eeb6cfa3d2d815bfc14f1cab5af
Umayir Feroze <umayir10@gmail.com>
2025-05-12 15:39:45 +1000

    Install development dependencies
    - Vite
    - Pnpm
    - HeadlessUI
    - Tailwind CSS

---
+++ web-app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+function Message(){
+    const name = 'Mosh'
+
+    if (name)
+        return <h1 className="text-3xl font-bold underline">Hello {name}!</h1>;
+    return <h1 className="text-3xl font-bold underline">Hello World!</h1>
+}
+
+export default Message;
