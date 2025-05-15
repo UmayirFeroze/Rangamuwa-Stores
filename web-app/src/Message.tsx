810c18fa60bdf177a800466566a7190ff5ec4d8e
Umayir Feroze <umayir10@gmail.com>
2025-05-15 14:27:36 +1000

    Update Message.tsx

--- web-app/src/Message.tsx
+++ web-app/src/Message.tsx
@@ -1,10 +1,11 @@
+import ListGroup from './components/ListGroup';
 
-function Message(){
-    const name = 'Mosh'
+function Message() {
+  const name = 'Mosh';
 
-    if (name)
-        return <h1 className="text-3xl font-bold underline">Hello {name}!</h1>;
-    return <h1 className="text-3xl font-bold underline">Hello World!</h1>
+  if (name)
+    // return <h1 className="text-3xl font-bold underline">Hello {name}!</h1>;
+    return <ListGroup />;
 }
 
 export default Message;
