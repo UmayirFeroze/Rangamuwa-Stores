6905e10f6abffba6facfa5bfab8868000799bfa6
Umayir Feroze <umayir10@gmail.com>
2025-05-20 18:23:39 +1000

    Refactor Menu Items Screen

--- web-app/src/components/MenuItems.tsx
+++ web-app/src/components/MenuItems.tsx
@@ -1,22 +1,18 @@
 
 import MenuItem from './MenuItem'
-import { BanknotesIcon, BookOpenIcon, Cog8ToothIcon, CurrencyDollarIcon, DocumentCurrencyDollarIcon, HomeIcon, PresentationChartLineIcon, ShoppingBagIcon, UserMinusIcon, UserPlusIcon } from '@heroicons/react/24/outline';
 
-const MenuItems = () => {
-  const items = [
-    { name: 'purchase', Icon: HomeIcon, onClick: () => alert('Clicked Purchase') },
-    {name: 'sales', Icon: BanknotesIcon, onClick: () => alert('Clicked Sales')},
-    {name: 'cash book', Icon: BookOpenIcon, onClick: () => alert('Clicked cash book')},
-    {name: 'accounts', Icon: DocumentCurrencyDollarIcon, onClick: () => alert('Clicked accounts')},
-    {name: 'buyers', Icon: UserPlusIcon, onClick: () => alert('Clicked buyers')},
-    {name: 'sellers', Icon: UserMinusIcon, onClick: () => alert('Clicked sellers')},
-    {name: 'items', Icon: ShoppingBagIcon, onClick: () => alert('Clicked items')},
-    {name: 'day price', Icon: CurrencyDollarIcon, onClick: () => alert('Clicked day price')},
-    {name: 'sales report', Icon: PresentationChartLineIcon, onClick: () => alert('Clicked sales report')},
-    {name: 'settings', Icon: Cog8ToothIcon, onClick: () => alert('Clicked settings')},
-    {name: 'settings', Icon: Cog8ToothIcon, onClick: () => alert('Clicked settings')},
-    {name: 'settings', Icon: Cog8ToothIcon, onClick: () => alert('Clicked settings')},
-  ]
+
+interface Item {
+  name: string;
+  Icon: React.ElementType;
+  onClick: () => void;
+}
+
+interface Props {
+  items: Item[];
+}
+
+const MenuItems = ({items}:Props) => {
 
   return (
     <div className="my-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4">
